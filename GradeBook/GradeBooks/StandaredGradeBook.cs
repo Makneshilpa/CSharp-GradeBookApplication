50b969e645f7f20ef51d86ca86a909d4c2227cdd
Makne shilpa <shilpamakne62@gmail.com>
2022-10-15 06:37:13 +0000

    GradeBooks

---
+++ GradeBook/GradeBooks/StandaredGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+   public class StandaredGradeBook : BaseGradeBook
+    {
+        public StandaredGradeBook(string name):base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
