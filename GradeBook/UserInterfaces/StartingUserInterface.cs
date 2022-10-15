81500d0f3001e9d5bbc1fe461b9aab2be829d18d
Makne shilpa <shilpamakne62@gmail.com>
2022-10-15 08:28:37 +0000

    StartingUserInterface

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -44,11 +44,11 @@ namespace GradeBook.UserInterfaces
             BaseGradeBook gradeBook;
             if(type == "Standard")
              gradeBook = new StandardGradeBook(name);
-            if (type == "Ranked")
+            else if (type == "Ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine(" {0}  is not a supported type of gradebook, please try again.", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
